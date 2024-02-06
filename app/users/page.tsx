31d0b96d1b595f18b63653531e9fd86bca46fb9b
olegKrainyk <oleg.kr944@gmail.com>
2024-02-06 18:13:32 -0500

    remove user in list
    using form instead of onclick, pathing id as prop

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,22 +2,35 @@ import { RemoveUser } from "@/components/RemoveUser/RemoveUser";
 import { Reveal } from "@/components/Reveal/reveal"
 import { PrismaClient, users } from '@prisma/client'
 import { revalidatePath } from "next/cache";
-import Link from "next/link";
+import Link from "next/link"
+import prisma from '../../lib/prisma'
+
 
 export default async function Users({}) {
-  
-    const prisma = new PrismaClient()
-    const data = await prisma.users.findMany()
-    revalidatePath('/users');
+  async function fetchUsers() {
+    const data = await prisma.users.findMany();
+    
+    return data;
+  }
+
+  const data = await fetchUsers();
+  revalidatePath('/users');
+    
 
     return (
       <main>  
             {data.map((user: users) => {
             return (
+
+              <>
+              <RemoveUser id={user.id}>
+                <div></div>
+              </RemoveUser>
             
               <Reveal key={user.id}>
                 <div >{user.login} --- pass: {user.password} ---- id: {user.id}</div>
               </Reveal>
+              </>
             );
             })}
             
--- components/RemoveUser/RemoveUser.tsx
+++ components/RemoveUser/RemoveUser.tsx
@@ -1,17 +1,36 @@
-"use client";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 import React from "react"
 
     interface Props {
         children: JSX.Element;
+        id: number;
     }
 
-    export const RemoveUser = ({ children}: Props) => {
-        
+    export const RemoveUser = ({ children, id}: Props) => {
+
+        const removeUser = async () => { 
+            "use server";
+    
+            try {
+                await prisma.users.delete({where: 
+                    {id: Number(id),
+                }}
+                );
+            
+              }catch (error) {
+                
+                return { error: 'Error deleting user' };
+              } finally {
+                revalidatePath('/users');
+              }
+        } 
 
         return (
-            <div>
-                --
+            <form action={removeUser}>
+                <input type="hidden" name="id" value={id} />
                 {children}
-            </div>
+                <button type="submit">-</button>
+            </form>
         );
 }
