8120444ddbb03f54666a019d6b6713a9ddcf7f91
olegKrainyk <oleg.kr944@gmail.com>
2024-02-10 18:43:46 -0500

    navigatio v 0.1

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -3,21 +3,48 @@
 import React from "react"
 import Link from "next/link"
 import style from "./style.module.css"
+import { motion } from "framer-motion"
 
 export default function Navigation() {
+
+    const [isOpen, setIsOpen] = React.useState(false)
+
+    const variants = {
+        active: {
+            x: '0',
+            transition: {
+                duration: 0.2,
+                ease: "easeInOut",
+            },
+        },
+        closed: {
+            x: "100%",
+            transition: {
+                duration: 0.2,
+                ease: "easeInOut",
+            },
+        },
+    }
+
   return (
-    <div className={style.wrapper}>
-        <nav className={style.navigation}>
-            <Link href="/" className={style.link}>
-                Home
-            </Link>
-            <Link href="/about" className={style.link}>
-                About
-            </Link>
-            <Link href="/users" className={style.link}>
-                Users
-            </Link>
-        </nav>
+    <div className={style.wrapperbig}>
+        <div onClick={() => setIsOpen(!isOpen)} className={style.btn}>
+                &#10006;
+        </div>
+        <motion.div animate={isOpen ? 'active' : 'closed'} initial={false} variants={variants} className={style.wrapper}>
+
+            <nav className={style.navigation}>
+                <Link href="/" className={style.link}>
+                    Home
+                </Link>
+                <Link href="/users" className={style.link}>
+                    Users
+                </Link>
+                <Link href="/users/addUser" className={style.link}>
+                    Add User
+                </Link>
+            </nav>
+        </motion.div>
     </div>
   );
 }
