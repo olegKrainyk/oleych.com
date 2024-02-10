ffdc83142fd67c87fcb16ba43fcf76885e85b831
olegKrainyk <oleg.kr944@gmail.com>
2024-02-09 20:57:33 -0500

    check if same username exist, error display

--- actions/addUser.ts
+++ actions/addUser.ts
@@ -4,19 +4,33 @@ import prisma from "@/lib/prisma"
 import { revalidatePath } from "next/cache"
 
 export const addUser = async(formData: FormData) => { 
-    
-    const userpicn: number = Math.floor(Math.random() * 9) + 1
 
-    try {
-        await prisma.users.create({data: {
-            username: formData.get('username') as string,
-            password: formData.get('password') as string,
-            userpic: userpicn,
+    try{
+        const userExists = await prisma.users.findUnique({
+            where: {
+                username: formData.get('username') as string,
+            }
+        })
+        if(userExists == null) {
+            const userpicn: number = Math.floor(Math.random() * 9) + 1
+            try {
+                await prisma.users.create({data: {
+                    username: formData.get('username') as string,
+                    password: formData.get('password') as string,
+                    userpic: userpicn,
+                }})
+            } catch (error) {
+                return { error: 'error :///' }
+            } finally {
+                revalidatePath('/users')
+            }
+        } else {
+            return { error: 'User already exists' }
         }
-    })
     } catch (error) {
-        return { error: 'Error adding user' };
+        return { error: 'error :///' };
     } finally {
-        revalidatePath('/users');
+        revalidatePath('/users')
+        revalidatePath('/users/addUser')
     }
 }
--- components/AddUserForm/AddUserForm.tsx
+++ components/AddUserForm/AddUserForm.tsx
@@ -4,14 +4,24 @@ import React from "react"
 import SubmitBtn from "../SubmitBtn/SubmitBtn"
 import { addUser } from "@/actions/addUser"
 import { useRef } from 'react'
+import style from './style.module.css'
+import { revalidatePath } from "next/cache";
 
 export default function AddUserForm() {
 
   const ref = useRef<HTMLFormElement>(null)
+  const [response, setResponse] = React.useState<string | null>('')
 
   const handleSubmit = async (formData: FormData) => {
-    await addUser(formData)
-    ref.current?.reset()
+    await addUser(formData).then((res) => {
+      if (res?.error) {
+       setResponse(res.error)
+      } else {
+        setResponse('')
+        ref.current?.reset()
+      }
+    })
+    
   }
 
     return (
@@ -19,16 +29,19 @@ export default function AddUserForm() {
             <div>
             <label>
               Username:
-              <input type="text" name="username" required />
+              <input type="text" name="username" maxLength={20} minLength={5} required />
             </label>
             </div>
             <div>
             <label>
               Password:
-              <input type="text" name="password" required/>
+              <input type="text" name="password" maxLength={30} minLength={8} required/>
             </label>
             </div>
-            <SubmitBtn text='Add user' class='btn'/>
+            <div className={style.submit}>
+              <SubmitBtn text='Add user' class='btn'/>
+              <div className={style.response}>{response}</div>
+            </div>
         </form>
     )
 }
